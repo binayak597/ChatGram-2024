e24c79f2af3dca2e1b4425cbcc3b7dce93997f16
Binayak Mukherjee <binayak.mukherjee9640@gmail.com>
2024-07-02 02:43:28 +0530

    add deployment configuration

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+import path from "path";
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
@@ -9,12 +10,15 @@ import messageRouter from './src/features/messages/messages.routes.js';
 import cors from "cors";
 import { app, server } from './socket/socket.js';
 
+const __dirname = path.resolve();
 
 app.use(cookieParser());
 
 app.use(express.json()); //parse the incoming req json payload data
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
 //api gateway
 
@@ -27,9 +31,14 @@ app.use("/api/user", userRouter);
 //http://localhost:8000/api/message
 app.use("/api/message", jwtAuth, messageRouter);
 
-app.get("/", (req, res) => res.send("welcome"));
 
-let PORT = process.env.PORT || 3200;
+app.use("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+// app.get("/", (req, res) => res.send("welcome"));
+
+let PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
 
--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -12,7 +12,7 @@ const generateTokenAndSetCookie = (userId, userEmail, res) => {
         maxAge: 15 * 24 * 60 * 60 * 1000, //MS
         httpOnly: "true", //prevents XSS attacks cross-site scripting attacks
         sameSite: "strict", //CSRF attacks cross-site request forgery attack
-        secure: process.env.NODE_ENV !== "development"
+        // secure: process.env.NODE_ENV !== "development"
 
         //basically any body who wants to access this token they can't able to access via javascript
     });
